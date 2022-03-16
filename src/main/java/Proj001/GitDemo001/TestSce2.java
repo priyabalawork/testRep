74c00db07908bf3944967c7b542a8e5aaf355595
Arpana <muriki.arpana@gmail.com>
2022-03-16 20:22:05 +0530

    Second Commit

---
+++ src/main/java/Proj001/GitDemo001/TestSce2.java
@@ -0,0 +1,12 @@
+package Proj001.GitDemo001;
+
+import org.testng.annotations.Test;
+
+public class TestSce2 {
+
+	@Test
+	public void test1()
+	{
+		System.out.println("This is TestNG class");
+	}
+}

51a4ec9c399ff81b958230a5cf2505273c2df53b
Arpana <muriki.arpana@gmail.com>
2022-03-16 20:19:42 +0530

    First Commit

---
+++ src/main/java/Proj001/GitDemo001/TestSce1.java
@@ -0,0 +1,10 @@
+package Proj001.GitDemo001;
+
+public class TestSce1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Welcome to Git Repository...");
+	}
+
+}
